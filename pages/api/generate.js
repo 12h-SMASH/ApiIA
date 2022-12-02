2854067dfc591a02630060c36bc5d96c3d34f52b
UP-4303 <up4303.pro@protonmail.com>
2022-12-02 02:23:15 +0100

    IA V1.0

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
 	const completion = await openai.createCompletion({
 		model: "text-davinci-003",
-    	prompt: generatePrompt(req.body.Question, req.body.Reponse),
+    	prompt: generatePrompt(req.body.question, req.body.answer),
     	temperature: 0.5,
     	max_tokens: 60,
     	top_p: 0.3,
@@ -19,14 +19,20 @@ export default async function (req, res) {
 }
 
 function generatePrompt(Q, A) {
-  	return `Trois personnes participent. Elles parlent toutes les trois uniquement en français. Les personnes sont IA, Humain et MJ. MJ commencera par poser une question à laquelle Humain va répondre. IA devra dire à Humain si sa réponse est la bonne, avec un ton très condescendant et particulièrement exaspéré.
-  	MJ: Combien de fois dois tu te masturber par jour pour être bon en sport?
-  	Human: 7 fois en moyenne.
-  	IA: Très bien ! Je suis surpris que tu connaisse celle là.
-  	MJ: Que signifie IST?
-  	Human: Information Super Terminal!
-  	IA: Je n'aurais pas dû en attendre plus.
-  	MJ: ${Q}
-	Human: ${A}
-  	IA:`;
+  	return `Une question et une réponse sont données ci-dessous. Juger leur corrélation avec condéscendance.
+Question: Que signifie IST ?
+Réponse: Infections Sexuellement Transmissibles
+Jugement: Mmh... c'est... juste.
+Question: Vrai ou Faux ? Les IST et les MST sont les mêmes choses.
+Réponse: Faux
+Jugement: C'est nul !
+Question: Vrai ou Faux ? Les femmes sont les plus touchées par la syphilis.
+Réponse: Faux
+Jugement: Mouais... ça passe...
+Question: Quelle est l’IST la plus fréquente ?
+Réponse: VIH
+Jugement: Pathétique !
+Question: ${Q}
+Réponse: ${A}
+Jugement:`;
 }
